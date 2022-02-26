59415105ddf707f1b3e162fd66c742094c952ea6
Tolya Fam <famovkin@yandex.ru>
2022-02-26 17:21:26 +0300

    feat: сверстать пустую корзину

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../components";
+import cartEmptyImage from "../images/cartEmptyImage.jpg";
 
 function Cart() {
-  return <h1>Корзина</h1>;
+  return (
+    <div className="container">
+      <div className="cart cart__type_empty">
+        <h1>Корзина пустая</h1>
+        <p>
+          Вероятней всего, вы не заказывали ещё пиццу. <br />
+          Для того, чтобы заказать пиццу, перейди на главную страницу.
+        </p>
+        <img src={cartEmptyImage} alt="Пустая корзина"></img>
+        <Link to="/">
+          <Button className="button__black">Вернуться назад</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
