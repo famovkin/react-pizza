0503120182ee2d9b740832617c30043df8ea4ffc
Tolya Fam <famovkin@yandex.ru>
2022-02-26 18:48:27 +0300

    feat: получить список пицц с помощью redux-thunk, подключить json-server

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import { useDispatch } from "react-redux";
 import { Route } from "react-router-dom";
 import { Header } from "./components";
 import { Cart, Home } from "./pages";
-import { setPizzas } from "./redux/actions/pizzas";
+import { fetchPizzas } from "./redux/actions/pizzas";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("http://localhost:3000/db.json")
-      .then((res) => res.json())
-      .then((data) => dispatch(setPizzas(data.pizzas)));
+    dispatch(fetchPizzas());
   }, [dispatch]);
 
   return (
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -2,3 +2,9 @@ export const setPizzas = (pizzas) => ({
   type: "SET_PIZZAS",
   payload: pizzas,
 });
+
+export const fetchPizzas = () => (dispatch) => {
+  fetch("http://localhost:3001/pizzas")
+      .then((res) => res.json())
+      .then((data) => (dispatch(setPizzas(data))));
+};
